01d8999c8f5345ef1f61fb1aacc9f772bc046a47
riteshkumar2 <riteshkumar2@deloitte.com>
2022-10-06 23:43:28 +0530

    Initial commit

---
+++ src/main/java/com/hashedin/momos/momosMania/MomosManiaApplication.java
@@ -0,0 +1,13 @@
+package com.hashedin.momos.momosMania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomosManiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomosManiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hashedin/momos/momosMania/MomosManiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashedin.momos.momosMania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomosManiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
